1444e7a0c972bbe6f655464acae75e4642d5f665
Aaron Magid <ahm64@case.edu>
2018-11-06 18:47:20 -0500

    chore(Setup): Added basic interface skeleton & routes.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const mountAPI = require('./api');
-const mountAuth = require('./auth');
 
 module.exports = addRoutes;
 
 function addRoutes(router) {
-    const api = express.Router(),
-        auth = express.Router();
+    const api = express.Router();
     mountAPI(api);
-    mountAuth(auth);
     
     router.use('/api', api);
-    router.use('/auth', auth);
+
+    router.get('/', (req, res) => res.sendFile(__dirname + "/main.html"));
+    router.get('/css', (req, res) => res.sendFile(__dirname + "/main.css"));
+    router.get('/js', (req, res) => res.sendFile(__dirname + "/main.js"));
 };
